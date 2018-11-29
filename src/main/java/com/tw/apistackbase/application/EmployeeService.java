729f8513b4ae4f35eb97fad07b3390765b178bec
Monique <moniquechan1220@gmail.com>
2018-11-29 15:48:13 +0800

    Completed

---
+++ src/main/java/com/tw/apistackbase/application/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.tw.apistackbase.application;
+
+import com.tw.apistackbase.Repository.EmployeeRepository;
+import com.tw.apistackbase.core.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    private static List<Employee> employees = new LinkedList<>(Arrays.asList(
+        new Employee("Xiaoming", 20),
+        new Employee("Xiaohong", 19),
+        new Employee("Xiaozhi", 15)
+    ));
+    public List<Employee> list() {
+        return (List<Employee>) employeeRepository.findAll();
+    }
+
+    public void add(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
